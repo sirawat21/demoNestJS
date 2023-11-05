528a881adfa012bb67634774e5af6ef41310508b
sirawat21 <sirawat21@gmail.com>
2023-11-05 23:11:55 +1100

    added condition for returning report type

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -2,18 +2,37 @@ import { Controller, Get, Post, Put, Delete, Param } from '@nestjs/common';
 
 import { data, EType, IData } from './reports.mockdata';
 
+// Controller for report
 @Controller('report/:type')
 export class AppController {
   @Get()
-  getAllReport(@Param('type') type: string) {
-    const result = data.report.filter((item) => item.type === EType.Income)
-    console.log(result);
+  getAllReport(@Param(EType.Income) reportType: string) {
+    if (reportType === EType.Income) {
+      const result = data.report.filter((item) => item.type === EType.Income);
+      console.log(result);
+    } else {
+      const result = data.report.filter((item) => item.type === EType.Expanse);
+      console.log(result);
+    }
     return [];
   }
 
   @Get(':id')
-  getReportById() {
-    return {};
+  getReportById(
+    @Param(EType.Income) reportType: string,
+    @Param('id') reportId: string,
+  ) {
+    if (reportType === EType.Income) {
+      const result = data.report.filter(
+        (item) => item.type === EType.Income && item.id === reportId,
+      );
+      console.log(result);
+    } else {
+      const result = data.report.filter((item) => item.type === EType.Expanse);
+      console.log(result);
+
+    }
+    return [];
   }
 
   @Post()
